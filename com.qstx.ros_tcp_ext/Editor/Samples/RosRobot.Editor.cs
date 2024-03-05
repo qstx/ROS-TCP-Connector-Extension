16555a443c6f1ba9ac8f2c33c2a7a47acdf3a7a2
qstx <2031074519@qq.com>
2024-03-05 16:04:31 +0800

    RosRobot组件添加角度制和弧度制切换功能

--- com.qstx.ros_tcp_ext/Editor/Samples/RosRobot.Editor.cs
+++ com.qstx.ros_tcp_ext/Editor/Samples/RosRobot.Editor.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEditor;
 
@@ -6,30 +7,50 @@ namespace QSTX.ROSTCPConnector.Extension.Editor
     [CustomEditor(typeof(RosRobot))]
     public class RosRobotEditor : UnityEditor.Editor
     {
+        private SerializedProperty degOrradProperty;
+        private SerializedProperty msgEventProperty;
+        private SerializedProperty topicNameProperty;
+        private SerializedProperty jointsProperty;
+        private SerializedProperty testMsgProperty;
+
+        private void OnEnable()
+        {
+            degOrradProperty = serializedObject.FindProperty("isDeg");
+            msgEventProperty = serializedObject.FindProperty("msgEvent");
+            topicNameProperty = serializedObject.FindProperty("topicName");
+            jointsProperty = serializedObject.FindProperty("joints");
+            testMsgProperty = serializedObject.FindProperty("msg");
+        }
+
         public override void OnInspectorGUI()
         {
+            serializedObject.Update();
             RosRobot t = target as RosRobot;
-            Undo.RecordObject(t, "Record");
-
-            base.OnInspectorGUI();
+            
+            if (degOrradProperty.boolValue && GUILayout.Button("Use Degrees"))
+                degOrradProperty.boolValue = false;
+            else if(!degOrradProperty.boolValue && GUILayout.Button("Use Radians"))
+                degOrradProperty.boolValue = true;
+            EditorGUILayout.PropertyField(msgEventProperty);
+            EditorGUILayout.PropertyField(jointsProperty);
+            EditorGUILayout.PropertyField(topicNameProperty);
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(testMsgProperty);
+            serializedObject.ApplyModifiedProperties();
+            
             if (!Application.isPlaying)
                 return;
             EditorGUILayout.BeginVertical();
             if (GUILayout.Button("Action"))
-            {
                 t.MoveToTarget(t.msg);
-            }
 
             if (!t.IsSubscribed)
             {
                 if(GUILayout.Button("Subscribe"))
                     t.Subscribe();
             }
-            else
-            {
-                if(GUILayout.Button("Unsubscribe"))
+            else if(GUILayout.Button("Unsubscribe"))
                     t.UnSubscribe();
-            }
 
             EditorGUILayout.EndVertical();
         }
--- com.qstx.ros_tcp_ext/Runtime/Samples/RosRobot.cs
+++ com.qstx.ros_tcp_ext/Runtime/Samples/RosRobot.cs
@@ -11,12 +11,13 @@ namespace QSTX.ROSTCPConnector.Extension
     public class RosRobot : RosTopicSubscriber<JointStateMsg>
     {
         [SerializeField] private ArticulationBody[] joints;
+        [SerializeField] private bool isDeg = true;
         public void MoveToTarget(JointStateMsg msg)
         {
             for (int i = 0; i < joints.Length; ++i)
             {
                 var driver=joints[i].xDrive;
-                driver.target = (float)msg.position[i];
+                driver.target = isDeg?(float)msg.position[i]:(float)msg.position[i]*Mathf.Rad2Deg;
                 joints[i].xDrive = driver;
             }
         }
